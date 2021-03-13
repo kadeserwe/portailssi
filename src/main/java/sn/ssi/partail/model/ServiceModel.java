f2062a47e45c27aef220b138fc2b9c0cf7a326ee
amote <amotekades@gmail.com>
2021-03-13 13:06:58 +0000

    send

--- src/main/java/sn/ssi/partail/model/ServiceModel.java
+++ src/main/java/sn/ssi/partail/model/ServiceModel.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 @Table(name = "service")
 public class ServiceModel implements Serializable {
     private static final long serialVersionUID = 1L;
-
+//
     @Id
     @GeneratedValue
     private  int id;
